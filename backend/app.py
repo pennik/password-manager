854718931ce86f339de8b8c240a32c924ff2be3c
pennik <niklas.schmitz2004@gmail.com>
2025-02-13 03:45:13 +0100

    feat: implement user creation route using flask  with bcrypt password hashing and MySQL integration

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 import mysql.connector
 from mysql.connector import Error
+from flask import Flask,request, jsonify
+import bcrypt
+import string
 
 try:
     # Verbindungsaufbau zur Datenbank
@@ -7,8 +10,71 @@ try:
         host='localhost',
         user='root',
         password='',
-        database='passwordmanager'
+        database='PasswordManager'
     )
     print("Connected Succesfully")
 except Error as e:
     print("Error on MYSQL connection", e)
+
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+def check_password(stored_hash, input_password):
+    return bcrypt.checkpw(input_password.encode('utf-8'), stored_hash)
+
+def is_valid_password(password: str) -> bool:
+
+    has_letter = any(char.isalpha() for char in password)  # Mindestens ein Buchstabe 
+    has_digit = any(char.isdigit() for char in password)   # Mindestens eine Zahl
+    has_special = any(char in string.punctuation for char in password)  # Sonderzeichen
+    
+    if len(password) < 8: return 0
+    if has_letter != True: return 0
+    if has_digit != True: return 0
+    if has_special != True: return 0 
+
+    return 1
+
+def getUser(username):
+    query = """
+    Select * From users
+    where Login_Name = %s
+    """
+    cursor = connection.cursor()
+    cursor.execute(query, (username,))
+    result = cursor.fetchall()
+    cursor.close()
+    return result
+
+def saveUser(last_name, first_name, password, login_name):
+    hashed_password = hash_password(password)
+    query="""
+    Insert into users (last_name, first_name, master_password_hash, login_name)
+    Value(%s, %s, %s, %s)
+    """
+    cursor = connection.cursor()
+    cursor.execute(query, (last_name, first_name, hashed_password, login_name))
+    connection.commit()
+    cursor.close()
+    return 1
+
+app = Flask(__name__)
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    last_name = data.get("last_name")
+    first_name = data.get("first_name")
+    password = data.get("password")
+    login_name = data.get("login_name")
+    
+    if is_valid_password(password) and getUser(login_name) == []: 
+        saveUser(last_name, first_name, password, login_name)
+        return "Successfully Created User", 201
+
+    elif is_valid_password != True: return "Check Passwords Requirements", 400
+
+if __name__ == "__main__":
+        app.run(debug=True)
