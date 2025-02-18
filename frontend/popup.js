9e8bfceb5866f385b213351c7119c0696797c323
pennik <niklas.schmitz2004@gmail.com>
2025-02-18 02:14:29 +0100

    feat: Working Login with token sotrage

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,14 +1,31 @@
-
-window.addEventListener('load', resizePopup);
-  
-document.getElementById('loginForm').addEventListener('submit', function (event) {
+document.getElementById('loginForm').addEventListener('submit',async function(event) {
     event.preventDefault(); // Verhindert das Neuladen der Seite
-    
+
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+
+    const data = {
+        login_name: username,
+        password: password
+    };
     
-    console.log('Benutzername:', username);
-    console.log('Passwort:', password);
-    
-    alert(`Login erfolgreich!\nBenutzername: ${username}`);
+    try {
+        const response = await fetch("http://127.0.0.1:5000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            const result = await response.json();
+            localStorage.setItem("token", result.token);
+        } else {
+            alert("Login fehlgeschlagen. Bitte überprüfe deine Eingaben.");
+        }
+    } catch (error) {
+        console.error("Fehler beim Login:", error);
+        console.log(result);
+        alert("Es gab ein Problem mit der Anfrage.");
+    }
 });
